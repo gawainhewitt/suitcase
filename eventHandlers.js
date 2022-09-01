a177431c953345eafb3b6c171db9ff3d0aea913a
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-09-01 19:11:05 +0100

    cleaning up for reinstall

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,7 +1,3 @@
-// const binders = {
-//   'bindMouseEnter' : 'stringIsPlucked'
-// } from John
-
 class EventHandlers {
   
   constructor(eventBinder, suitcaseSoundControl) {
@@ -12,16 +8,6 @@ class EventHandlers {
     this.mouseEnterCount = 0;
     this.buttonCount = 0;
     this.mouseDown = false;
-    
-    // for (const property in binders) {
-    //   const handler = this[binders[property]]
-    //   this.eventBinder[property] = handler
-    // }
-
-    // [].forEach((el) => {
-
-    // }) From John
-
     this.eventBinder.bindMouseEnter(this.stringIsPlucked);
     this.eventBinder.bindSelectStart(this.disableSelect);
     this.eventBinder.bindMouseDown(this.registerMouseDown);
@@ -44,7 +30,6 @@ class EventHandlers {
       if(this.chordButtonState[i]){
         console.log("are we here?")
         document.querySelector(chordBlockClasses[i]).style.display = "flex";
-        // john suggesting I could instead create and destroy a class here rather than change the style
       }else{
         document.querySelector(chordBlockClasses[i]).style.display = "none";
       }
@@ -52,7 +37,7 @@ class EventHandlers {
   }
 
   displayStartButton = () => {
-    this.eventBinder.bindStartScreen(this.hideStartScreen); // get this actioned once sounds have loaded
+    this.eventBinder.bindStartScreen(this.hideStartScreen); 
   }
 
   hideStartScreen = () => {
@@ -96,23 +81,20 @@ class EventHandlers {
     e.preventDefault();
     console.log("touch start");
     let touches = e.changedTouches;
-    // console.log(touches);
     this.ongoingTouches.push(this.#copyTouch(touches[0]));
-    // console.log(this.ongoingTouches);
     this.#showElement(this.ongoingTouches);
   }
 
   #handleTouchEnd = (e) => {
     e.preventDefault(); 
     let touches = e.changedTouches; 
-    // console.log("touch end");
 
     for (let i = 0; i < touches.length; i++) {
       let idx = this.#ongoingTouchIndexById(touches[i].identifier); 
-      if (idx >= 0) { // did we get a match?
+      if (idx >= 0) { 
         console.log("touchend "+idx);
-        this.ongoingTouches.splice(idx, 1);  // remove it; we're done
-      } else { // no match
+        this.ongoingTouches.splice(idx, 1);  
+      } else { 
         console.log("can't figure out which touch to end");
       }
       for(let j = 0; j < this.touchesOnElements.length; j++){
@@ -129,10 +111,9 @@ class EventHandlers {
     let touches = e.changedTouches;
     for (let i = 0; i < touches.length; i++) {
       let idx = this.#ongoingTouchIndexById(touches[i].identifier); 
-      if (idx >= 0) { // did we get a match?
+      if (idx >= 0) { 
         this.ongoingTouches.splice(idx, 1, this.#copyTouch(touches[i]));
-        // console.log(this.#copyTouch(touches[i]));  
-      } else { // no match
+      } else { 
         console.log("can't figure out which touch to continue");
       }
     }
@@ -146,7 +127,7 @@ class EventHandlers {
   
     for (let i = 0; i < touches.length; i++) {
       let idx = this.ongoingTouchIndexById(touches[i].identifier); 
-      this.ongoingTouches.splice(idx, 1);  // remove it; we're done
+      this.ongoingTouches.splice(idx, 1);  
     }
   }
 
@@ -161,13 +142,12 @@ class EventHandlers {
         return i;
       }
     }
-    return -1;    // not found
+    return -1;    
   }
 
   #showElement = () => {
     for(let i = 0; i < this.ongoingTouches.length; i++){
       let el = document.elementFromPoint(this.ongoingTouches[i].clientX, this.ongoingTouches[i].clientY);
-      // console.log(`element = ${el.id}`);
       
       if(this.#isNewTouchOnElement(i, el.id)){
         for(let i = 0; i < 3; i++){
@@ -182,15 +162,11 @@ class EventHandlers {
   }
 
   #isNewTouchOnElement = (idx, el_id) => {
-    // console.log(`length of this touches on elements = ${this.touchesOnElements.length}`)
     for(let i = 0; i < this.touchesOnElements.length; i++){
-      // console.log(`touches on elements ${i} ${this.touchesOnElements[i]}`);
       if(this.touchesOnElements[i].touch_id === this.ongoingTouches[idx].identifier){
         if(this.touchesOnElements[i].element_id === el_id){
-          // console.log("already on element");
           return false;
         }else{
-          // console.log("same touch new element");
           this.touchesOnElements.splice(i, 1, {touch_id: this.ongoingTouches[idx].identifier, element_id: el_id});
           return true;
         }
